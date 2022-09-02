a0a45e7a9a7009adaf686ad56ba9d0566a4317c6
mdmuhibul <112680631+mdmuhibul@users.noreply.github.com>
2022-09-02 01:18:06 -0400

    Add files via upload

---
+++ HW 1 (Md Muhibul Islam).R
@@ -0,0 +1,29 @@
+Homework One
+
+Group  1
+Econ  B2000 
+
+Md  Muhibul Islam, Minghao, Amira 
+
+
+load("C:/Users/Muhibul/Downloads/Documents/CCNY/Household_Pulse_data (1).RData")
+#glimpse(acs2017_ny) try this later
+Household_Pulse_data[1:10,1:7
+summary(TBIRTH_YEAR[GENID_DESCRIBE == "female"]
+summary(TBIRTH_YEAR[GENID_DESCRIBE == "male"]           
+summary(TBIRTH_YEAR[GENID_DESCRIBE == "transgender"])
+
+summary(TBIRTH_YEAR[GENID_DESCRIBE == "other"
+
+summary(TBIRTH_YEAR[GENID_DESCRIBE == "NA"]
+
+# here i want to find average ages of men and women
+mean(TBIRTH_YEAR[GENID_DESCRIBE == "female"])
+
+sd(TBIRTH_YEAR[GENID_DESCRIBE == "female"])
+
+mean(TBIRTH_YEAR[GENID_DESCRIBE == "male"])
+
+sd(TBIRTH_YEAR[GENID_DESCRIBE == "male"])
+
+Summary: To conclude, I would like to say that higher educated people are cautious about covid and they are willing to get vaccinated.
